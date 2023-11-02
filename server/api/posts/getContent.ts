6282d8691e21c9e0833f96e388ed6acddbfff220
Cksheuen <116556497+Cksheuen@users.noreply.github.com>
2023-11-02 13:34:54 +0800

    __dirname
    更改路径

--- server/api/posts/getContent.ts
+++ server/api/posts/getContent.ts
@@ -1,14 +1,22 @@
 import fs from 'node:fs'
-import path from 'node:path'
+import path, { dirname } from 'node:path'
 import process from 'node:process'
+import { fileURLToPath } from 'node:url'
 import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+console.log(__dirname)
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const postsDirectory = path.join(process.cwd(), `posts/${body.path}`)
+  const postsDirectory = path.join(__dirname, '..', '..', 'posts', body.path)
+  // path.join(process.cwd(), `posts/${body.path}`)// `@/posts/${body.path}`
+  console.log(postsDirectory)
+
   const fullPath = path.join(postsDirectory, `${body.id}.md`)
   const fileContent = fs.readFileSync(fullPath, 'utf8')
   const matterResult = matter(fileContent)
--- server/api/posts/postDirs.ts
+++ server/api/posts/postDirs.ts
@@ -1,12 +1,21 @@
-import path from 'node:path'
+import path, { dirname } from 'node:path'
 import process from 'node:process'
 import fs from 'node:fs'
+import { fileURLToPath } from 'node:url'
 import matter from 'gray-matter'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+// console.log('path.join(__dirname, \'..\', \'..\') -- ', path.join(__dirname, '..', '..'))
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const postsDirectory = path.join(process.cwd(), `posts/${body.path}`)
+  const postsDirectory = path.join(__dirname, '..', '..', 'posts', body.path)
+  // path.join(process.cwd(), `posts/${body.path}`)
+  // `@/posts/${body.path}`
+  console.log(postsDirectory)
+
   const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map((fileName) => {
     const id = fileName.replace('/\.md$/', '')
