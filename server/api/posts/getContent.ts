5eebe332735cacd1c4cb9474d81659436a4d7c7d
Cksheuen <116556497+Cksheuen@users.noreply.github.com>
2023-11-02 16:29:23 +0800

    。。。

--- server/api/posts/getContent.ts
+++ server/api/posts/getContent.ts
@@ -13,7 +13,8 @@ const __dirname = dirname(__filename)
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const postsDirectory = `./public/posts/${body.path}`
+  const postsDirectory
+  //  = `./public/posts/${body.path}`
   = path.join(process.cwd(), 'public', 'posts', body.path)
   // = path.join(__dirname, '..', 'public', 'posts', body.path)
   // = path.join(process.cwd(), `posts/${body.path}`)
